13cf0f4129ec58564de2edf60ec05762b3fc858a
NayrXr <75831864+NayrXr@users.noreply.github.com>
2024-06-17 02:21:46 +0700

    Delete CLOUD_COMPUTING/CLOUD_COMPUTING directory

--- CLOUD_COMPUTING/CLOUD_COMPUTING/src/pages/BareMinimum/CustomerSegmentation/data/datacustomersegmentation.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useState } from 'react';
-
-const fetchData = async () => {
-    const response = await fetch('https://ps01rfm-g463lwzijq-et.a.run.app/api/customer-segments-table');
-    const dataMBA = await response.json();
-    return dataMBA;
-};
-
-export default fetchData;
